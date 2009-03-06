99890158a98185aaa24b1ea89ad69607c4baf6ea
Yaohan Chen <yaohan.chen@gmail.com>
2009-03-06 15:56:48 -0500

    refactor specs
    use methods like "any_value", "any_method_name" instead of putting
    concrete values in each spec

--- spec/chatter_spec.rb
+++ spec/chatter_spec.rb
@@ -3,30 +3,47 @@ require 'chatter'
 describe Chatter do
   before(:each) do
     @chatter = Class.new
+    @chatter.send(:attr_accessor, :value)
     @chatter.extend Chatter
   end
 
-  it "should act like define_method when given one sub-method" do
-    @chatter.chat(:test_method) {'correct_value'}
-    @chatter.new.test_method.should == 'correct_value'
+  def any_value
+    rand
+  end
+
+  def any_function
+    proc do |*a|
+      a.reverse
+    end
+  end
+
+  def any_method_name
+    :"test_method_#{rand(100)}"
+  end
+
+  it "should define the method using block when only one sub-method" do
+    v = any_value
+    m = any_method_name
+    @chatter.chat(m) {v}
+    @chatter.new.send(m).should == v
   end
 
   it "should evaluate method block in instance scope" do
-    @chatter.chat(:test_method) {@a}
-    @chatter.new.tap do |o|
-      o.instance_variable_set(:@a, 'correct_value')
-    end.test_method.should == 'correct_value'
+    v = any_value
+    m = any_method_name
+    @chatter.chat(:test_method) {@value}
+    @chatter.new.tap {|o| o.value = v}.test_method.should == v
   end
 
   it "should combine multiple sub-methods to function like one method" do
-    method_body = proc do |a1, a2, a3|
-      [a1, a3] * 2 + [a2]
-    end
-    @chatter.chat(:test_method_1, :test_method_2, :test_method_3, &method_body)
-    @chatter.send(:define_method, :equiv_method, &method_body) 
+    method_body = any_function
+    (m1, m2, m3) = Array.new(3) {any_method_name}
+    (v1, v2, v3) = Array.new(3) {any_value}
+    @chatter.chat(m1, m2, m3, &method_body)
+    @chatter.send(:define_method, :equiv_method, &method_body)
     @chatter.new.tap do |o|
-      o.test_method_1(1).test_method_2(2).test_method_3(3).should ==
-        o.equiv_method(1, 2, 3)
+      o.send(m1, v1).send(m2, v2).send(m3, v3).should ==
+        o.equiv_method(v1, v2, v3)
     end
   end
 end
